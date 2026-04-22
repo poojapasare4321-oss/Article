9c9eff16329e860b1833291b3b3a52eabfd092ee
POOJA PASARE <poojapasare4321@gmail.com>
2026-04-22 19:05:08 +0530

    fixed dns duplicate error

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,24 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
+// import dns from 'dns';
+
+// const globalForPrisma = globalThis
+// dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// const dns = require("dns")
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+
+ import { PrismaClient } from '@prisma/client'
 import dns from 'dns';
 
-const globalForPrisma = globalThis
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-const dns = require("dns")
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const globalForPrisma = globalThis;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
 
-
- 
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.prisma = prisma;
